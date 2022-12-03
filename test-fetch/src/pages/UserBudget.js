c11effa437b9ef3e1fc7700d066317d65a7f0793
caominhbao444 <113426992+caominhbao444@users.noreply.github.com>
2022-12-03 19:55:09 +0700

    update

--- test-fetch/src/pages/UserBudget.js
+++ test-fetch/src/pages/UserBudget.js
@@ -1,13 +1,35 @@
 import React from "react";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
+import { Tabs } from "antd";
+import Overview from "../components/Overview";
+import { BankOutlined } from "@ant-design/icons";
 import "../Css/userBudget.css";
+import { Link } from "react-router-dom";
 const UserBudget = () => {
   return (
     <>
       <NavBar />
       <section className="userBudget">
-        <h2>Budget</h2>
+        <Tabs defaultActiveKey="1" tabPosition="left" className="userBudget">
+          <Tabs.TabPane tab="Overview" key="1">
+            <Overview />
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Fiat and Spot" key="2" id="fiatspot">
+            Content of Tab Pane 2
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Futures" key="3">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Funding" key="4">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Transaction History" key="5">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+        </Tabs>
       </section>
+      <Footer />
     </>
   );
 };
